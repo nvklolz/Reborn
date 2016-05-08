01c0768407bef5464d944001281d487b0d7d1843
nvklolz <kevin.nunes@icloud.com>
2016-05-08 17:24:29 +0200

    add topicController

---
+++ src/WebsiteBundle/Controller/TopicController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class TopicController extends Controller
+{
+    /**
+     * @Route("/forum/{id}")
+     */
+    public function topicAction($id)
+    {
+        $headTopic = $this->getDoctrine()
+                ->getRepository('WebsiteBundle:HeadTopic')
+                ->find($id);
+
+        $topics = $this->getDoctrine()
+            ->getRepository('WebsiteBundle:Topics')
+            ->findAll();
+
+
+        return $this->render('WebsiteBundle:Forum:topic.html.twig',
+            array('topics' => $topics,
+                  'headTopic' => $headTopic
+            ));
+    }
+}
