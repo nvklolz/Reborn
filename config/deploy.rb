d4ae8e6fbb2f96cadd665d79bfb8bdf376a3be57
nvklolz <kevin.nunes@icloud.com>
2016-05-30 23:16:50 +0200

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,14 +35,50 @@ set :repo_url, 'git@github.com:nvklolz/Reborn.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :symfony_env,  "prod"
+
+# Set this to 2 for the old directory structure
+set :symfony_directory_structure, 3
+# Set this to 4 if using the older SensioDistributionBundle
+set :sensio_distribution_version, 5
+
+# symfony-standard edition directories
+set :app_path, "app"
+set :web_path, "web"
+set :var_path, "var"
+set :bin_path, "bin"
+
+# The next 3 settings are lazily evaluated from the above values, so take care
+# when modifying them
+set :app_config_path, "app/config"
+set :log_path, "var/logs"
+set :cache_path, "var/cache"
+
+set :symfony_console_path, "bin/console"
+set :symfony_console_flags, "--no-debug"
+
+# Remove app_dev.php during deployment, other files in web/ can be specified here
+set :controllers_to_clear, ["app_*.php"]
+
+# asset management
+set :assets_install_path, "web"
+set :assets_install_flags,  '--symlink'
+
+# Share files/directories between releases
+set :linked_files, ["app/config/parameters.yml"]
+set :linked_dirs, ["var/logs"]
+
+# Set correct permissions between releases, this is turned off by default
+set :file_permissions_paths, ["var"]
+set :permission_method, false
+
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  after :updated, :composer do
+    on roles(:web) do
+      within release_path do
+        execute :composer, :install
+        end
     end
   end
 
